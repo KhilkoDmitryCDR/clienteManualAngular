53def86ba85e1116a56c5b5d64b60ba7c0a76f70
Dmitry Khilko <dmitry.jilko@gmail.com>
2021-10-01 12:48:26 +0200

    Práctica Angular con Módulos, Componentes y Servicios DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ClientesModule} from './clientes/clientes.module';
+import {ListadoClientesComponent} from "./clientes/listado-clientes/listado-clientes.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/clientes/listado-clientes/listado-clientes.component.ts
+++ src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -9,7 +9,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ListadoClientesComponent implements OnInit {
 
-  clientes: Cliente[];
+  clientes!: Cliente[];
 
   constructor(private clientesService: ClientesService) {
   }
